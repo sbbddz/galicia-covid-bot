438c9ff2aa247676e4ef30b85ec63f38a063b018
sbbddz <bdzsamuel@gmail.com>
2021-03-02 12:54:22 +0100

    fixed bugs and added ESP date

--- lang_module.py
+++ lang_module.py
@@ -1,4 +1,6 @@
 import json
+from datetime import datetime
+
 
 class Lang():
 
@@ -8,3 +10,13 @@ class Lang():
 
     def get_lang_file(self):
         return self.lang
+
+    @staticmethod
+    def parse_date_eu(date):
+        return datetime.strptime(
+            date, "%Y-%m-%d").strftime("%d-%m-%Y")
+
+    @staticmethod
+    def parse_date_us(date):
+        return datetime.strptime(
+            date, "%d-%m-%Y").strftime("%Y-%m-%d")
--- sergas_info.py
+++ sergas_info.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from data_parse import Data
 from lang_module import Lang
+
 
 class Sergas():
     def __init__(self):
@@ -17,18 +19,19 @@ class Sergas():
         return formatted_string
 
     def get_total_cases_day(self, date):
-        data = self.data.fetch_data(date=date)
+        data = self.data.fetch_data(Lang().parse_date_us(date))
         if data == None:
             return "Ahora mismo no tenemos datos disponibles"
         formatted_string = self.lang["day-header"].format(date)
         for i, j in enumerate(data['Casos_Totais'].values()):
-            formatted_string += self.lang["total-cases"].format(self.data.get_health_area(i), str(j))
-        
+            formatted_string += self.lang["total-cases"].format(
+                self.data.get_health_area(i), str(j))
+
         return formatted_string
 
     def get_new_cases_lastday(self):
         formatted_string = self.lang["day-header"].format(self.lastday)
         for i, j in enumerate(self.fetched_data['Casos_Confirmados_PCR_Ultimas24h'].values()):
-            formatted_string += self.lang["new-cases"].format(self.data.get_health_area(i), str(j))
+            formatted_string += self.lang["new-cases"].format(
+                self.data.get_health_area(i), str(j))
         return formatted_string
-
